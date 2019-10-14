3cc4a978a5f8ba9b6225a050e5606e050356a911
sergiolondon2410 <sergiolondon2410@gmail.com>
2019-10-14 15:47:21 -0500

    model created

--- tutorial1.py
+++ tutorial1.py
@@ -11,7 +11,19 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 train_images = train_images/255.0
-train_test = train_test/255.0
-print(train_images[7])
-plt.imshow(train_images[7], cmap=plt.cm.binary)
-plt.show()
+test_images = test_images/255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam",
+              loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Tested Acc: ", test_acc)
