4194f30e821b8e9987a08bed9e8cee2c6293a4ab
sergiolondon2410 <sergiolondon2410@gmail.com>
2019-10-15 14:11:24 -0500

    predictions were made

--- tutorial1.py
+++ tutorial1.py
@@ -24,6 +24,52 @@ model.compile(optimizer="adam",
 
 model.fit(train_images, train_labels, epochs=10)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+# test_loss, test_acc = model.evaluate(test_images, test_labels)
 
-print("Tested Acc: ", test_acc)
+# print("Tested Acc: ", test_acc)
+
+predictions = model.predict(test_images)
+print(predictions[0])
+print(np.argmax(predictions[0]))
+
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+i = 0
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
